2807b3db41928b88f795761da2b6e0042cb96205
duhrice <ivan.chen223@gmail.com>
2025-09-11 23:06:57 -0400

    Added debuff modifier types

--- pkg/modifer/modifier.go
+++ pkg/modifer/modifier.go
@@ -23,12 +23,11 @@ const (
 	IncomingDmgDown		// Incoming damage down
 	DefUp				// Defense up
 	EvaUp				// Evasion up
-	FocusedAssault		// Focused assault
 	ConRecovUp			// Continuous recovery up
 	RecovBstUp			// Recovery boost up
 	HealingUp			// Healing up
 	AccUp				// Accuracy up
-	MaxHpUp				// MaxHpUp
+	MaxHpUp				// Max hp up
 	SpdUp				// Speed up
 	CCResistUp			// Crowd control resistance up
 	CCPwrUp				// Crowd control power up
@@ -54,10 +53,59 @@ const (
 	CostRegenUp			// Cost regeneration up
 	CostOverload		// Cost overload
 	DmgDealtUp			// Damage dealt up
-	DefPen				// Defense peneration
 	ExDmgDealtUp		// EX skill damage dealt up
 	IncomingExDmgDown	// Incoming EX skill damage down
 	// Debuffs
+	AtkDown				// Attack down
+	AtkSpdDown			// Attack speed down
+	BlkRateDown			// Block rate down
+	BulletTypeDmgDown	// Bullet type damage down
+	Bleed				// Bleed
+	Burn				// Burn
+	BurnDmgUp			// Burn damage up
+	Chill				// Chill
+	ChillDmgUp			// Chill damage up
+	CostUp				// Cost increase
+	CostRegenDown		// Cost regeneration down
+	CritRateDown		// Critical rate down
+	CritResistDown		// Critical resistance down
+	CritDmgDown			// Critical damage down
+	CritDmgResistDown	// Critical damage resistance down
+	Weakness			// Weakness
+	IncomingDmgUp		// incoming damage up
+	DefDown				// Defense down
+	EvaDown				// Evasion down
+	ConRecovDown		// Continuous recovery down
+	RecovBstDown		// Recovery boost down
+	HealingDown			// Healing down
+	Curse				// Curse
+	AccDown				// Accuracy down
+	MaxHpDown			// Max hp down
+	SpdDown				// Speed down
+	CCResistDown		// Crowd control resistance down
+	Poison				// Poison
+	PoisonDmgUp			// Poison damage up
+	RngDown				// Range down
+	StabDown			// Stability down
+	Unconscious			// ??? (what kind of debuff is this???)
+	ViewDown			// View down
+	DecreaseExplo		// Decrease explosion damage
+	DecreasePier		// Decrease piercing damage
+	DecreaseMyst		// Decrease mystic damage
+	DecreaseSonic		// Decrease sonic damage
+	DecreaseLightArm	// Decrease light armor
+	DecreaseHeavyArm	// Decrease heavy armor
+	DecreaseSpecArm		// Decrease special armor
+	DecreaseElasArm		// Decrease elastic armor
+	HealingLimitDown	// Healing limit down
+	Cheer				// Cheer
+	ElecShock			// Electric shock
+	ElecShockDmgUp		// Eelctric shock damage up
+	ExDmgDealtDown		// EX skill damage dealt down
+	IncomingExDmgUp		// Incoming EX skill damage up
+	// Both
+	FocusedAssault		// Focused assault
+	DefPen				// Defense peneration
 	// Crowd control
 	// Special
 )
